cf1a56db05ebea855ed4bad5f850c8c7ded94358
adamtheplanetarium <adam@theplanetarium.net>
2026-01-01 02:42:44 -0800

    Switch from eventlet to gevent for Python 3.13 compatibility

--- start_web.sh
+++ start_web.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")/backend"
-exec gunicorn --worker-class eventlet -w 1 --bind 0.0.0.0:$PORT app:app
+exec gunicorn --worker-class gevent -w 1 --bind 0.0.0.0:$PORT app:app
